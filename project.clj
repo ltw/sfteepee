0767534d55ab259098530102d185632b8f92585f
Wilkes Joiner <wilkesjoiner@gmail.com>
2011-08-16 21:03:26 -0500

    bump to 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.wilkes/sfteepee "0.3.0"
+(defproject org.clojars.wilkes/sfteepee "0.4.0"
   :description "A clojure wrapper for Jsch SFTP"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.jcraft/jsch "0.1.42"]])
